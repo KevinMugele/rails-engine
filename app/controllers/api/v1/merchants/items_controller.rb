4b2f40cdac6e20a27124bd6dc87a8d97f4886294
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-24 17:21:22 -0400

    Create namespace for merchants items controller. Add index method for merchants items

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    items = merchant.items
+    render json: ItemSerializer.new(items)
 
-end 
+  rescue ActiveRecord::RecordNotFound
+    no_merchant_error
+  end
+end

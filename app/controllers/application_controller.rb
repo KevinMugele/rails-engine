c3e5837794b9a4a1975f1b53bcbeb87fb93a30e3
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:40:49 -0400

    Change layout of exceptions. Added Exception Handler to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::API
-  def no_merchant_error
-    render json: {
-        message: "your query could not be completed",
-        errors: ["no merchant found with id: #{params[:id]}"],
-      }, status: 404
-  end
+  include ExceptionHandler
 end

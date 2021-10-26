9c5ca4cc7d8044df208d39f301699514a43e4f4e
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:42:26 -0400

    Add relationships and validations for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,10 @@
 class Invoice < ApplicationRecord
+  belongs_to :customer
+  belongs_to :merchant
+
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
+  has_many :transactions, dependent: :destroy
+
+  validates :status, presence: true
 end

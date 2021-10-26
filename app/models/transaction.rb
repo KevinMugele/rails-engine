4f5df4c2cd15d873c83b1314e1cf8fd7cb533371
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:42:49 -0400

    Add relationships and validations for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
+
+  validates :credit_card_number, presence: true,
+                                 numericality: true,
+                                 length: { in: 15..16 }
+  validates :credit_card_expiration_date, presence: true
+  validates :result, presence: true
 end

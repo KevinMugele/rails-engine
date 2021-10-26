13c357ad051ea36f2df418ca5ff97028a6c7fb03
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:40:16 -0400

    Add handrolled routes for find and find_all merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/api/v1/merchants/find', to: 'api/v1/merchants#find'
+  get '/api/v1/merchants/find_all', to: 'api/v1/merchants#find_all'
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
         resources :items, only: :index, controller: 'merchants/items'
       end
+
+      resources :items do
+        resources :merchant, only: :index, controller: 'items/merchants'
+      end
     end
   end
+
 end

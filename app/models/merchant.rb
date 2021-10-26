4ef3c290b4ee60ed6d68fa319f41aa36713541e2
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:42:44 -0400

    Add relationships and validations for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,26 @@
 class Merchant < ApplicationRecord
-  has_many :items
+  has_many :invoices, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :invoice_items, through: :items
+
+  validates :name, presence: true
+
+  def self.find_all_by_name(name = nil)
+    return nil if name.nil?
+
+    where('name ILIKE ?', "%#{name}%")
+      .order_by_name
+  end
+
+  def self.find_by_name(name = nil)
+    return nil if name.nil?
+
+    order_by_name
+      .find_by('name ILIKE ?', "%#{name}%")
+  end
+
+  def self.order_by_name(order = 'asc')
+    order = 'asc' unless order == 'desc'
+    order(name: order)
+  end
 end

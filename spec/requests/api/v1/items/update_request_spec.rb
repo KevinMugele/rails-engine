972082d2fff3d051c8d72fd2978a024f2443dbea
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-26 10:39:25 -0400

    Write test for merchants update request spec

---
+++ spec/requests/api/v1/items/update_request_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'update item API' do
+  it 'updates item' do
+    merchant = create(:merchant)
+    merchant_items = create_list(:item, 15, merchant: merchant)
+    id = Item.last.id
+    previous_name = Item.last.name
+    item_params = {
+      name: 'lamp'
+    }
+    headers = {"CONTENT_TYPE": "application/json"}
+
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item: item_params)
+
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("lamp")
+  end
+end

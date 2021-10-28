9dd86339cb1f284cec1bb13abc98d51e32289a73
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-28 10:55:17 -0400

    Refactor exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,32 +2,33 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordInvalid do |e|
+    rescue_from ActionController::BadRequest do |e|
       render json: {
-        message: 'Invalid Record',
-        errors: { details: e.message }
-      }, status: :not_found
-    end
+        message: 'Invalid Request',
+        error: { details: e.message }
+        }, status: 400
+      end
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: {
         message: 'your query could not be completed',
-        errors: { details: e.message }
+        error: { details: e.message }
       }, status: :not_found
     end
 
-    rescue_from ActionController::ParameterMissing do |e|
-      render json: {
-        message: 'Missing Parameter',
-        errors: { details: e.message }
-      }, status: 400
-    end
+    # rescue_from ActiveRecord::RecordInvalid do |e|
+    #   render json: {
+    #     message: 'Invalid Record',
+    #     error: { details: e.message }
+    #   }, status: :not_found
+    # end
 
-    rescue_from ActionController::BadRequest do |e|
-      render json: {
-        message: 'Invalid Request',
-        errors: { details: e.message }
-      }, status: 400
-    end
+    # rescue_from ActionController::ParameterMissing do |e|
+    #   render json: {
+    #     message: 'Missing Parameter',
+    #     error: { details: e.message }
+    #   }, status: 400
+    # end
+
   end
 end

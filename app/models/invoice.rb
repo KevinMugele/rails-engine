41ca201e55b72a1e0c69f824e49908f38f700831
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-28 10:55:09 -0400

    Add scope for shipped invoices, methods for invoices considered revenue and unshipped order revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,4 +7,23 @@ class Invoice < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
   validates :status, presence: true
+
+  scope :shipped, -> {
+    where(status: 'shipped')
+  }
+
+  def self.considered_revenue
+    joins(:transactions)
+      .where(invoices: { status: 'shipped' },
+             transactions: { result: 'success' })
+  end
+
+  def self.unshipped_order_revenue(quantity = 10)
+      joins(:invoice_items)
+      .where(status: 'packaged')
+      .select('invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS potential_revenue')
+      .group(:id)
+      .order(potential_revenue: :desc)
+      .limit(quantity)
+  end
 end

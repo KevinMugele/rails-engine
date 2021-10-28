bc50ff41ccf72e20b642d64b1655c85ee737df0b
Kevin Mugele <kevin.mugele@gmail.com>
2021-10-28 10:54:48 -0400

    Add two methods to find all items by searched name

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
+
+  def self.find_all_by_name(name)
+    where('name ILIKE ?', "%#{name}%")
+      .order_by_name
+  end
+
+  def self.order_by_name(order = 'asc')
+    order = 'asc' unless order == 'desc'
+    order(name: order)
+  end
 end
